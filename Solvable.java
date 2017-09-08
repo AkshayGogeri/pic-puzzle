85f9f707a2a73b4ffbc1e646fbd022eb3b1c95c1
Akshay Gogeri <akshay.gogeri@gmail.com>
2017-09-09 04:54:14 +0530

    java interface for this game

---
+++ Solvable.java
@@ -0,0 +1,12 @@
+interface Solvable
+{  public void createList();
+   public void createPuzzle();
+   public void shuffle();
+   public void Listswap(String x);
+   public void adjustpuz();
+   public boolean isCrctShuffled();
+   public boolean isComplete();
+   public void solvePuzzle();
+   public void listTraverse(Node x);
+
+}
